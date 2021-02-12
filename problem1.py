bd275a83b9e09b70357783cc5c4ab66f85a4dc17
Lucas Pei <lucas.pei23@ycdsbk12.ca>
2021-02-12 14:21:06 +0000

    finished live hack 2 practice

--- problem1.py
+++ problem1.py
@@ -1,1 +1,27 @@
+"""
+********************************************************
+Filename:       problem1.py
+Description:    Determine if a triangle is a right angle triangle
+Author:         Pei.L
+Created On:     12/02/2021
+********************************************************
+"""
+print("******Right Angle Triangle Calculator******")
 
+#get side lengths
+side1 = int(input("Side 1 length: "))
+side2 = int(input("Side 2 length: "))
+side3 = int(input("Side 3 length: "))
+
+#calculate squares
+square1 = side1**2
+square2 = side2**2
+square3 = side3**2
+
+#determine if it is a right triangle
+if (square1+square2 == square3) or (square1+square3 == square2) or (square2+square3 == square1):
+  print("A triangle with lengths " + str(side1) + ", " +
+    str(side2) + ", and " + str(side3) + " forms a right-angled triangle.")
+else:
+    print("A triangle with lengths " + str(side1) + ", " +
+    str(side2) + ", and " + str(side3) + " does not form a right-angled triangle.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,21 @@
+"""
+********************************************************
+Filename:       problem2.py
+Description:    Determine where you get to go on summer vacation
+Author:         Pei.L
+Created On:     12/02/2021
+********************************************************
+"""
+print("******Summer Vacation Destination Calculator******")
 
+#ask for final grade and earnings
+final_grade = float(input("What is your final grade?: "))
+earnings = float(input("How much money did you earn?: "))
+
+#calculate and find out destination
+if final_grade >= 80 and earnings >= 500:
+  print("Congrats! You are going to Europe!")
+elif final_grade >= 80 and earnings < 500:
+  print("You are going to California!")
+else: 
+  print("You have to stay at home.")
